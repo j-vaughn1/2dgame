2f3ec8c4badabed95a1f7639395539c5e53e779c
Duy Pham <duypham93@gmail.com>
2018-02-20 15:13:46 -0700

    Add Main Menu
    Add new scene "Main Menu".
    All scene now save in the folder "Scene"

---
+++ CatVsDog_Unity/Assets/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+    public void Play()
+    {
+        //Goto Game scene
+    }
+
+    public void Options() 
+    {
+        //Goto Option Scence
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
