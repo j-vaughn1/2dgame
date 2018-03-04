f18fe67a25d9a8ae2b344dca3eaac7ab46613dda
j-vaughn1 <jordan.a2.vaughn@ucdenver.edu>
2018-03-04 12:35:40 -0700

    Bone Update
    Some updates on the Ammo script and bone object. The bone explodes on a timer now (than can change), so it's more like a bomb. The bone also has a force explosion that can be tweaked, feel free to play with it.

--- CatVsDog_Unity/Assets/Scripts/Ammo.cs
+++ CatVsDog_Unity/Assets/Scripts/Ammo.cs
@@ -3,49 +3,82 @@ using UnityEngine;
 
 public class Ammo : MonoBehaviour {
 
-    public GameObject boneAmmo;
-    public GameObject explosionBone;
+    public GameObject explosion;             //uses prefab of explosion
+    public float explosion_delay = 1f;
+    public float current_size = 0f;
+    public float explosion_max_size = 5f;
+    public float explosion_rate = 1f;
+    public float explosion_speed = 1f;
+    bool exploded = false;
+    CircleCollider2D explosion_size;
 
+    //**************************
 
-
-    void OnExplode()
+    void Start()
     {
-        int power = 5;
-        int radius = 2;
+        explosion_size = gameObject.GetComponent<CircleCollider2D>();
+        
 
-        Vector3 explosionPos = transform.position;
-        Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
-        foreach (Collider hit in colliders)
+    }
+
+    //**************************
+
+    void Update()                        //a timer for the explosion
+    {
+
+        explosion_delay -= Time.deltaTime;
+        if(explosion_delay < 0)
         {
-            Rigidbody rb = hit.GetComponent<Rigidbody>();
-
-            if (rb != null)
-                rb.AddExplosionForce(power, explosionPos, radius, 3.0F);
-        }
-        Destroy(gameObject);
-            /*
-            Quaternion randomRotation = Quaternion.Euler(0f, 0f, Random.Range(0f, 360f));
-            Instantiate(explosionBone, transform.position, randomRotation); // Particle effect post explosion
-            Destroy(boneAmmo, 1);
-            Destroy(explosionBone, 2);
-            */
+            exploded = true;
         }
 
-    
+    }
 
-        void Collision2D(Collision col)
+    //*****************************
+
+    void FixedUpdate()                                     //physics of the explosion is updated here
     {
-            if (col.gameObject.tag == "Floor")
+        if(exploded == true)
+        {
+            if (current_size < explosion_max_size)         //explosion increases in size until max size is reached
             {
-                OnExplode();
+                current_size += explosion_rate;
             }
-
-            if (col.gameObject.tag == "Wall")
+            else
             {
-                OnExplode();
+                Quaternion randomRotation = Quaternion.Euler(0f, 0f, Random.Range(0f, 360f));
+                Instantiate(explosion, transform.position, randomRotation); // Particle effect post explosion
+                Object.Destroy(this.gameObject.transform.parent.gameObject);    //when max size is reached, object is destroyed
+            }
+            explosion_size.radius = current_size;
+        }
+    }
+
+
+    //*****************************
+
+    void onTriggerEnter2D(Collision col)                               //bone explosion physics
+    {
+        if(exploded == true)
+        {
+
+            if(col.gameObject.GetComponent<Rigidbody2D>() != null)     //explosion force
+            {
+                Vector2 target = col.gameObject.transform.position;
+                Vector2 bomb = gameObject.transform.position;
+
+                Vector2 direction = 5f * (target - bomb);
+                Rigidbody2D rb = col.gameObject.GetComponent<Rigidbody2D>();
+
+                rb.AddForce(new Vector2(direction.x, direction.y *2f));  //split the force between x and y components so either can be adjusted
+
             }
 
         }
-    
+
+
+    }
+
+
 }
 
